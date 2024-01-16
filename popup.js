819502ce9c63389cdf10aa3650a3f71e0bf3e3c3
felabs1 <felixawere01@gmail.com>
2024-01-17 00:56:04 +0300

    popup skeleton

---
+++ popup.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", function () {
+  /// in that we are going to target the element
+  document
+    .getElementById("accountList")
+    .addEventListener("click", changeAccount);
+  document.getElementById("userAddress").addEventListener("click", copyAddress);
+  document
+    .getElementById("header_network")
+    .addEventListener("click", getOpenNetwork);
+  document.getElementById("transferFund").addEventListener("click", handler);
+  document
+    .getElementById("network_item")
+    .addEventListener("click", getSelectedNetwork);
+  document.getElementById("add_network").addEventListener("click", setNetwork);
+  document.getElementById("loginAccount").addEventListener("click", loginUser);
+  document.getElementById("loginAccount").addEventListener("click", loginUser);
+  document.getElementById("loginAccount").addEventListener("click", loginUser);
+  document.getElementById("loginAccount").addEventListener("click", loginUser);
+  document.getElementById("loginAccount").addEventListener("click", loginUser);
+});
+
+// state variable
+let providerURL = "";
+let provider;
+let privateKey;
+let address;
+
+//function
+function handler() {}
+
+function checkBalance() {}
+
+function getOpenNetwork() {}
+
+function getSelectedNetwork() {}
+
+function setNetwork() {}
+
+function loginUser() {}
+
+function createUser() {}
+
+function openCreate() {}
+
+function signUp() {}
+
+function login() {}
+
+function logout() {}
+
+function openTransfer() {}
+
+function goBack() {}
+
+function openImport() {}
+
+function importGoBack() {}
+
+function openActivity() {}
+
+function openAssets() {}
+
+function goHomePage() {}
+
+function openImportModel() {}
+
+function closeImportModel() {}
+
+function addToken() {}
+
+function addAccount() {}
+
+function myFunction() {}
+
+function copyAddress() {}
+
+function changeAccount() {}
